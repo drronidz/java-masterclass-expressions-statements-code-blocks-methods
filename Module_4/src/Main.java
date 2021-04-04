f0e3aeef3b1fa04741ceea213f1e0da9b644f871
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-04 16:03:56 +0100

    issue done. closes #2

---
+++ Module_4/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // In the following code that I will type below, write down what parts of the code
+        // are expressions.
+
+        int score = 100;
+        if(score > 99) {
+            System.out.println("You got the high score ");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-masterclass-expressions-statements-code-blocks-methods
 Java Programming Masterclass for Software Developers ( CHAPITRE 4: Java Tutorial Expressions, Statements, Code blocks, Methods and more)
+List of java key words : 
+https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
